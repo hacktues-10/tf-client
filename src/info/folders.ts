be4b091f6a06a88ba3c76eb87431a6cecbbfcb64
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-01-31 23:52:58 +0200

    i sam voinut e voint

--- src/info/folders.ts
+++ src/info/folders.ts
@@ -1,49 +1,62 @@
 import tf2023 from './folders_images/tf2023_ekip.jpg';
 import tf2023_2 from './folders_images/tf2023_battlebots.jpg';
 import tf2022 from './folders_images/tf2022_img1.jpg';
-import tf2021_2 from './folders_images/tf2022_img2.jpg';
+import tf2022_2 from './folders_images/tf2022_img2.jpg';
+import tf2021 from './folders_images/tf2021_img1.jpg';
+import tf2021_2 from './folders_images/tf2021_img2.png';
+import tf2020_2 from './folders_images/tf2020_img2.jpg';
+import tf2020 from './folders_images/tf2020_img1.png';
+import tf2019_2 from './folders_images/tf2019_img2.jpg';
+import tf2019 from './folders_images/tf2019_img1.jpg';
+import tf2018_2 from './folders_images/tf2018_img2.jpg';
+import tf2018 from './folders_images/tf2018_img1.jpg';
+import tf2017_2 from './folders_images/tf2017_img2.jpg';
+import tf2017 from './folders_images/tf2017_img1.jpg';
+import tf2016_2 from './folders_images/tf2016_img2.jpg';
+import tf2016 from './folders_images/tf2016_img1.jpg';
+
 export const FOLDERS = [
 	{
 		id: 1,
 		name: '2016',
-		image1: tf2023,
-		image2: tf2023,
+		image1: tf2016,
+		image2: tf2016_2,
 	},
 	{
 		id: 2,
 		name: '2017',
-		image1: tf2023,
-		image2: tf2023,
+		image1: tf2017,
+		image2: tf2017_2,
 	},
 	{
 		id: 3,
 		name: '2018',
-		image1: tf2023,
-		image2: tf2023,
+		image1: tf2018,
+		image2: tf2018_2,
 	},
 	{
 		id: 4,
 		name: '2019',
-		image1: tf2023,
-		image2: tf2023,
+		image1: tf2019,
+		image2: tf2019_2,
 	},
 	{
 		id: 5,
 		name: '2020',
-		image1: tf2023,
-		image2: tf2023,
+		image1: tf2020,
+		image2: tf2020_2,
 	},
 	{
 		id: 6,
 		name: '2021',
-		image1: tf2023,
-		image2: tf2023,
+		image1: tf2021,
+		image2: tf2021_2,
 	},
 	{
 		id: 7,
 		name: '2022',
 		image1: tf2022,
-		image2: tf2021_2,
+		image2: tf2022_2,
 	},
 	{
 		id: 8,
