eb0df9a1bb52ad9f22d2e3c8aff106f3481812e9
hacktues-10-shared <141483383+hacktues-10-shared@users.noreply.github.com>
2024-02-15 11:35:42 +0200

    Update SullyAnimation.tsx

--- src/app/SullyAnimation.tsx
+++ src/app/SullyAnimation.tsx
@@ -2,7 +2,7 @@ import SullyGravity from './SullyGravity';
 import './animation.css';
 export default function SullyAnimation() {
 	return (
-		<div className="absolute top-0 left-0 w-screen h-screen overflow-hidden">
+		<div className="absolute top-0 left-0 w-full h-screen overflow-hidden">
 			<SullyGravity className="z-0 image h-[20%] absolute" />{' '}
 		</div>
 	);
