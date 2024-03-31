e20196b2cd56574290d7889da353092522b56cb1
bvpav <78971592+bvpav@users.noreply.github.com>
2024-03-31 17:54:10 +0300

    Enabled signout

--- src/app/register/reset/page.tsx
+++ src/app/register/reset/page.tsx
@@ -1,14 +1,27 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { signOut } from 'next-auth/react';
 
 export default function ResetPage() {
 	const router = useRouter();
+	const searchParams = useSearchParams();
 	useEffect(() => {
 		localStorage.removeItem('registrationDataCurrentStep');
 		localStorage.removeItem('registrationData');
-		router.replace('/register');
+		const signout = searchParams.get('signout');
+		if (signout === '1') {
+			signOut().then(() => router.replace('/login'));
+		} else {
+			router.replace('/register');
+		}
 	});
-	return null;
+	return (
+		<div className="flex h-screen w-full flex-col items-center justify-center">
+			<h2 className="bg-gradient bg-clip-text text-5xl font-black text-transparent">
+				Регистрацията е прекратена...
+			</h2>
+		</div>
+	);
 }
