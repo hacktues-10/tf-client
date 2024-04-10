be6718d2c2e7f22524ab01f78cd97375edc676ae
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-04-10 17:33:23 +0300

    maybe fixed it now?

--- src/app/SullyAnimation.tsx
+++ src/app/SullyAnimation.tsx
@@ -4,8 +4,8 @@ import './animation.css';
 
 export default function SullyAnimation() {
 	return (
-		<div className="absolute left-0 top-0 h-screen w-screen overflow-hidden">
-			<SullyGravity className="image pointer-events-none absolute z-0 h-[20%]" />{' '}
+		<div className="pointer-events-none absolute left-0 top-0 h-screen w-screen overflow-hidden">
+			<SullyGravity className="image absolute z-0 h-[20%]" />{' '}
 		</div>
 	);
 }
