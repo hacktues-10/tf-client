2ed0404665dcef307ed746052c7e56ece4331cc5
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-04-12 00:54:23 +0300

    trqbva da bachka na safari?

--- src/app/projects/category/[category]/page.tsx
+++ src/app/projects/category/[category]/page.tsx
@@ -1,13 +1,21 @@
 // IMPORTATN - THIS IS THE DUMBES SOLUTION, BUT I WANT SWEEEET SERVER COMPONENTS
 import { Suspense } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import { Card, CardHeader } from '@/components/ui/card';
 import ProjectsPath from '@/partials/layout/ProjectsPath';
 import ProjectsLoading from '@/partials/projects/loader/ProjectsLoading';
-import { Card, CardHeader } from '@/components/ui/card';
-import Image from 'next/image';
 import { FaYoutube } from 'react-icons/fa';
-import { getBotsProjects, getEmbeddedProjects, getNetworkinProjects, getProjects, getSoftwareProjects } from '../../actions';
+
+import {
+	getBotsProjects,
+	getEmbeddedProjects,
+	getNetworkinProjects,
+	getProjects,
+	getSoftwareProjects,
+} from '../../actions';
+
 const PATH: {
 	name: string;
 	url: string;
@@ -61,8 +69,7 @@ const LinkTab = ({ text, href, current }: { text: string; href: string; current:
 	</Link>
 );
 
-
-const ProjectsPage =async ({
+const ProjectsPage = async ({
 	params,
 }: {
 	params: {
@@ -71,7 +78,7 @@ const ProjectsPage =async ({
 }) => {
 	const { category } = params;
 
-	console.log("sadasdas", category)
+	console.log('sadasdas', category);
 
 	// if category not in [software, embedded, battlebots, networks, all]
 	// redirect to /projects
@@ -82,19 +89,17 @@ const ProjectsPage =async ({
 
 	let projects;
 
-	if (category == 'all'){
-	projects = await getProjects();
-	}else if(category == 'software'){
+	if (category == 'all') {
+		projects = await getProjects();
+	} else if (category == 'software') {
 		projects = await getSoftwareProjects();
-	}else if(category == 'embedded'){
-		projects =await getEmbeddedProjects();
-	}else if(category == 'networks'){
+	} else if (category == 'embedded') {
+		projects = await getEmbeddedProjects();
+	} else if (category == 'networks') {
 		projects = await getNetworkinProjects();
-	}
-	else if (category == "battlebot"){
+	} else if (category == 'battlebot') {
 		projects = await getBotsProjects();
 	}
-
 
 	projects?.sort(() => Math.random() - 0.5);
 
@@ -102,54 +107,73 @@ const ProjectsPage =async ({
 		<div className="container">
 			<ProjectsPath path={PATH} />
 			<div className="mb-28">
-			<section className="pt-8">
-							<div className="mx-4">
-								<Card className='opacity-100 bg-black text-white m-4 mb-14 rounded-lg border-2 border-stroke px-5 py-4'>
-									<div className="-mx-4 flex z-50 flex-wrap items-center justify-between">
-										<div className="w-full px-4">
-											<div className="flex flex-wrap justify-center gap-4 overflow-x-auto lg:justify-start">
-												{TABS.map((tab) => (
-													<LinkTab key={tab.href} {...tab} current={tab.category === category} />
-												))}
-											</div>
-										</div>
+				<section className="pt-8">
+					<div className="mx-4">
+						<Card className="m-4 mb-14 rounded-lg border-2 border-stroke bg-black px-5 py-4 text-white opacity-100">
+							<div className="z-50 -mx-4 flex flex-wrap items-center justify-between">
+								<div className="w-full px-4">
+									<div className="flex flex-wrap justify-center gap-4 overflow-x-auto lg:justify-start">
+										{TABS.map((tab) => (
+											<LinkTab key={tab.href} {...tab} current={tab.category === category} />
+										))}
 									</div>
-								</Card>
+								</div>
 							</div>
-						</section>
+						</Card>
+					</div>
+				</section>
 				{/* <ErrorBoundary FallbackComponent={ProjectsError}> */}
 				<Suspense fallback={<ProjectsLoading />}>
-					<div className='w-full sm:m-4 inline-grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-						{projects ? projects.map((project) => <Project key={project.title} project={project}/>) : <div>Loading...</div>}
-						</div>				</Suspense>
+					<div className="inline-grid w-full grid-cols-1 sm:m-4 md:grid-cols-2 lg:grid-cols-3">
+						{projects ? (
+							projects.map((project) => <Project key={project.title} project={project} />)
+						) : (
+							<div>Loading...</div>
+						)}
+					</div>{' '}
+				</Suspense>
 			</div>
 		</div>
 	);
 };
 
+const Project = ({ project }: { project: Awaited<ReturnType<typeof getProjects>>[number] }) => {
+	return (
+		<Card className="z-20 m-4 max-w-[500px] bg-black text-white opacity-100">
+			<div className="relative mx-auto mt-4 w-[90%]" style={{ paddingTop: '56.25%' }}>
+				<Image
+					key={project.id}
+					src={`https://pub-40c3b6cf3326458d9e34b64cd71f902c.r2.dev/${project.thumbnail == '' ? project.images.split(', ')[0] : project.thumbnail}`}
+					alt={project.title}
+					className="absolute left-0 top-0 rounded-lg object-cover"
+					layout="fill"
+					objectFit="cover"
+				/>
+			</div>
+			<CardHeader className="flex flex-row items-center justify-between">
+				<Link
+					className="text-xl font-semibold hover:cursor-pointer hover:text-sand"
+					href={`/projects/${project.id}`}
+				>
+					{project.title}
+				</Link>
+				{project.youtubeId && (
+					<YoutubeLink
+						href={`https://www.youtube.com/watch?v=${encodeURIComponent(project.youtubeId ?? '')}`}
+					/>
+				)}
+			</CardHeader>
+		</Card>
+	);
+};
 
-const Project = ({project} :{project: Awaited<ReturnType<typeof getProjects>>[number]}) => {
-	return <Card className='max-w-[500px] opacity-100 bg-black text-white m-4'>
-    <div className='relative w-[90%] mx-auto mt-4' style={{ paddingTop: '56.25%' }}>
-	<Image 
-	key={project.id}
-	src={`https://pub-40c3b6cf3326458d9e34b64cd71f902c.r2.dev/${project.thumbnail == "" ? project.images.split(", ")[0] : project.thumbnail}`} 
-	alt={project.title}
-	className='absolute top-0 left-0 object-cover rounded-lg'
-	layout='fill'
-	objectFit='cover'
-	/>
-    </div>
-    <CardHeader className='flex items-center flex-row justify-between'>
-      <Link className='text-xl font-semibold hover:text-sand hover:cursor-pointer' href={`/projects/${project.id}`}>{project.title}</Link>
-
-	  {project.youtubeId && <YoutubeLink href={`https://www.youtube.com/watch?v=${encodeURIComponent(project.youtubeId ?? "")}`}/>}    </CardHeader>
-  </Card>}
-
-const YoutubeLink = ({href} : {href: string}) => {
-	return <div className='m-1 p-1 rounded-lg hover:text-error hover:scale-110 duration-100'>
-	<Link href={href}>
-	  <FaYoutube size={32}/>
-  </Link>
-  </div>}
+const YoutubeLink = ({ href }: { href: string }) => {
+	return (
+		<div className="m-1 rounded-lg p-1 duration-100 hover:scale-110 hover:text-error">
+			<Link href={href}>
+				<FaYoutube size={32} />
+			</Link>
+		</div>
+	);
+};
 export default ProjectsPage;
