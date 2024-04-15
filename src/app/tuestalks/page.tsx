5515f2c760985ca739017fe9125d6ba7ef04d3c6
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-04-15 10:07:23 +0300

    spotify iframe

--- src/app/tuestalks/page.tsx
+++ src/app/tuestalks/page.tsx
@@ -2,20 +2,16 @@ const TUESTalksPage = () => (
 	<div className="relative flex min-h-screen items-center justify-center !pt-28">
 		<div className="relative flex h-full w-full items-center justify-center">
 			<iframe
-				id="embedPlayer"
-				src="https://embed.podcasts.apple.com/us/podcast/tues-talks/id1589981184?itsct=podcast_box_player&amp;itscg=30200&amp;ls=1&amp;theme=dark"
-				height="450px"
+				className=" w-[90%] max-w-[660px] overflow-hidden rounded-xl sm:w-[40%] "
+				src="https://open.spotify.com/embed/show/2HGIZmqHAFtNwfXOrlTu7v?utm_source=generator"
+				height="352"
+				style={{
+					animation: '2s ease 0s 6 normal none running loading-indicator',
+				}}
 				sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
 				allow="autoplay *; encrypted-media *; clipboard-write"
-				className="border border-stroke"
-				style={{
-					width: '100%',
-					maxWidth: '660px',
-					overflow: 'hidden',
-					borderRadius: '15px',
-					animation: '2s ease 0s 6 normal none running loading-indicator',
-					backgroundColor: 'rgb(228, 228, 228)',
-				}}
+				frameBorder="0"
+				loading="lazy"
 			></iframe>
 		</div>
 	</div>
