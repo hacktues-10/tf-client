71d27ef9e217455e37913ab540991921373da30e
bvpav <78971592+bvpav@users.noreply.github.com>
2024-04-21 10:15:59 +0300

    feature flags failed me

--- src/partials/layout/Voting.tsx
+++ src/partials/layout/Voting.tsx
@@ -334,10 +334,12 @@ const VotingLayout = () => {
 	}
 
 	return (
-		<IfTfFeatureOn feature="tf-vote-projects">
+		// <IfTfFeatureOn feature="tf-vote-projects">
+		<>
 			{showOverlay && !showModal && <VotingOverlay showModal={continueVoting} />}
 			{showModal && <VotingModal closeModal={closeModal} />}
-		</IfTfFeatureOn>
+		</>
+		// </IfTfFeatureOn>
 	);
 };
 
