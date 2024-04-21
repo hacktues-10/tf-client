0574ee4173c9f411b0fe32ff5375f6cc9f467b99
bvpav <78971592+bvpav@users.noreply.github.com>
2024-04-21 06:51:23 +0300

    default

--- src/app/headers/route.ts
+++ src/app/headers/route.ts
@@ -1,6 +1,6 @@
 import { headers } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
-export default function GET(_: NextRequest) {
+export function GET(_: NextRequest) {
 	return NextResponse.json(headers());
 }
